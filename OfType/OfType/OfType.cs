ba1e2a745c02a37bf74e13f840634fa8ce5b3e7d
Nadav Naveh <nadav.naveh.23@gmail.com>
2025-06-25 18:17:08 +0300

    Implemented GetOfType function

---
+++ OfType/OfType/OfType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfType
+{
+    public class OfType : IOfType
+    {
+        public IEnumerable<T> GetOfType<T>(IEnumerable enumerable)
+        {
+            List<T> result = new List<T>();
+            foreach (var item in enumerable)
+            {
+                if (item.GetType() == typeof(T))
+                {
+                    result.Add((T)item);
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
